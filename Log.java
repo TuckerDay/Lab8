7a2b08aa6788c72adcc1f76ac4ab1f4945e7dfbb
TuckerDay <37190942+TuckerDay@users.noreply.github.com>
2018-04-07 23:25:52 -0600

    Add files via upload

---
+++ Log.java
@@ -0,0 +1,8 @@
+/*
+ Catey Meador
+ Created maze files
+ Chose wall image
+ Added createWalls() in Game class to get walls to appear
+ Added page.drawImage for walls in MainPanel
+ Changed how mapArray was populated
+ */
